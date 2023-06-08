290fbdc194853a698c4d175dad4cfb6f6ba93386
johana cardenas <jjcardenas88@misena.edu.co>
2023-06-07 20:41:39 -0500

    feat:configuracion conexion

--- conexion/conectar.php
+++ conexion/conectar.php
@@ -3,14 +3,17 @@ class Conexion{
                
 
                private $servidor = "tiendatecnologica.mysql.database.azure.com";
-               private $usuario = "tiendatecnologica";
+               private $usuario = "admitilo";
                private $clave = "Tienda*123456";
                private $db = "tienda_tecnologica";
 
-
+               
                public function conectando() {
-                   $con = mysqli_connect($this->servidor,$this->usuario,$this->clave,$this->db) or die ("Error al conectar con el servidor");
-                   return $con;
+                 $conn = mysqli_init();
+               mysqli_ssl_set($conn,NULL,NULL, "/db/DDL_db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
+                   $conn = mysqli_connect($this->servidor,$this->usuario,$this->clave,$this->db) or die ("Error al conectar con el servidor");
+                   return $conn;
                }
 
 
