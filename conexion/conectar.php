b75a681c17c5970d22f1285eb6e352830a875ad3
johana cardenas <jjcardenas88@misena.edu.co>
2023-06-07 20:59:22 -0500

    feat:intento

--- conexion/conectar.php
+++ conexion/conectar.php
@@ -10,7 +10,7 @@ class Conexion{
                
                public function conectando() {
                  $conn = mysqli_init();
-               mysqli_ssl_set($conn,NULL,NULL, "/db/DDL_db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+               mysqli_ssl_set($conn,NULL,NULL, "../db/DDL_db/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 
                    $conn = mysqli_connect($this->servidor,$this->usuario,$this->clave,$this->db) or die ("Error al conectar con el servidor");
                    return $conn;
@@ -24,3 +24,11 @@ $obj = new Conexion();
  }
 
  ?>
+ <?php 
+//$conn = mysqli_init();
+//mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+//mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', 'myadmin', 'yourpassword', 'quickstartdb', 3306, MYSQLI_CLIENT_SSL);
+//if (mysqli_connect_errno()) {
+//die('Failed to connect to MySQL: '.mysqli_connect_error());
+//}
+?>
